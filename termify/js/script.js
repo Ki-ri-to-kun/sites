8a8e12aad6569fd49ddd0240cb5cfaeecff9b5ab
Ki-ri-to-kun <staskazakov9@gmail.com>
2021-07-16 12:49:14 +0600

    termify add

---
+++ termify/js/script.js
@@ -0,0 +1,43 @@
+
+function testWebP(callback) {
+
+var webP = new Image();
+webP.onload = webP.onerror = function () {
+callback(webP.height == 2);
+};
+webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+if (support == true) {
+document.querySelector('body').classList.add('webp');
+}else{
+document.querySelector('body').classList.add('no-webp');
+}
+});
+
+/*
+new Vue({
+	el: '',
+	data: {
+		
+		}	
+	},
+	methods: {
+		
+	}
+	
+});
+*/
+/*
+var sidebarLinks = document.querySelectorAll('.dashboard-sidebar ul li');
+for(var i=0; i<sidebarLinks.length; i++){
+	sidebarLinks[i].addEventListener('click', function(){
+		for(var y=0; y<sidebarLinks.length; y++){
+			sidebarLinks[y].classList.remove('dash-main-menu__item--active');
+		}
+		this.classList.add('dash-main-menu__item--active');
+	});
+}
+*/
