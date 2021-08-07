b20f7be445af5c6d73c290a87ecd9269b53c59de
Ki-ri-to-kun <staskazakov9@gmail.com>
2021-08-07 12:48:22 +0600

    small fix

--- termify/js/script.js
+++ termify/js/script.js
@@ -130,7 +130,7 @@ backdrop.addEventListener('click', function(){
 		secondBurgerLine.style.visibility = "visible";
 });
 
-
+/*
 window.addEventListener('scroll', function(){
 	
 	const target = document.querySelector('.slider-main__slider img');
@@ -147,38 +147,51 @@ window.addEventListener('scroll', function(){
 	sliderTitle.style.transform = "translate3d(0px, " + rate + "px, 0px)";
 	starRating.style.transform = "translate3d(0px, " + rate + "px, 0px)";
 	splidePagination.style.transform = "translateX(-50%) translate3d(0px, " + rate + "px, 0px)";
-});
+});*/
 
-/*
+
 var buttons = document.querySelectorAll('button');
 console.log(buttons);
 for (var i = 0; i <buttons.length; i++){
 	buttons[i].addEventListener('click', function(){
 		console.log('Кнопка нажата!');
 	});
-}*/
-
-/*
-new Vue({
-	el: '',
-	data: {
-		
-		}	
-	},
-	methods: {
-		
-	}
-	
-});
-*/
-/*
-var sidebarLinks = document.querySelectorAll('.dashboard-sidebar ul li');
-for(var i=0; i<sidebarLinks.length; i++){
-	sidebarLinks[i].addEventListener('click', function(){
-		for(var y=0; y<sidebarLinks.length; y++){
-			sidebarLinks[y].classList.remove('dash-main-menu__item--active');
-		}
-		this.classList.add('dash-main-menu__item--active');
-	});
 }
-*/
+
+document.addEventListener( 'DOMContentLoaded', function () {
+	// Vanilla JavaScript
+jarallax(document.querySelectorAll('.jarallax'), {
+	// scroll, scale, opacity, scale-opacity, scroll-opacity
+	type: 'scroll',
+	
+	// animation speed
+	speed: 0.5, 
+	
+	// image parallax scroll effect
+	imgSrc: null,
+	imgElement: '.jarallax-img',
+	imgSize: 'cover',
+	imgPosition: '50% 50%',
+	imgRepeat: 'no-repeat', 
+	
+	// keep <img> tag in it's default place
+	keepImg: false, 
+	
+	// use custom DOM / jQuery element to check if parallax block in viewport.
+	elementInViewport: null,
+	
+	// z-index property
+	zIndex: -100,
+	
+	// disable parallax scroll effect on certain devices
+	// e.g. disableParallax: /iPad|iPhone|iPod|<a href="https://www.jqueryscript.net/tags.php?/Android/">Android</a>/
+	disableParallax: false,
+	
+	// use ResizeObserver API to recalculate position and size of parallax image
+	automaticResize: true, 
+	
+	 });
+});
+
+
+
